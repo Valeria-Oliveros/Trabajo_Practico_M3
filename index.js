c050e708a5839428e52a726f643780690edb3582
Valeria <corredorv@uninorte.edu.co>
2026-04-05 19:08:36 -0500

    Cambio index.js

--- index.js
+++ index.js
@@ -9,26 +9,21 @@
 import express from 'express';
 import dotenv from 'dotenv';
 import cors from 'cors';
-import usersRoutes from './routers/users.routes.js';
-import itemsRoutes from './routers/items.routes.js';
 import path from 'path';
 import { fileURLToPath } from 'url';
-
-const __dirname = path.dirname(fileURLToPath(import.meta.url));
-app.use(express.static(path.join(__dirname, 'public')));
+import usersRoutes from './routers/users.routes.js';
+import itemsRoutes from './routers/items.routes.js';
 
 dotenv.config();
 
 const app = express();
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/users', usersRoutes);
 app.use('/items', itemsRoutes);
-
-app.get('/', (req, res) => {
-  res.send("API funcionando 🚀");
-});
 
 app.listen(process.env.PORT || 5000, () => {
   console.log("Servidor corriendo en puerto " + process.env.PORT);
