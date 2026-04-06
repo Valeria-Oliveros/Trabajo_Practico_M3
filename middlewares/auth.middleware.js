af926dd03d6721329f099113258edc1457cc70f0
Valeria <corredorv@uninorte.edu.co>
2026-04-05 19:14:49 -0500

    Cambio auth.middleware.js

--- middlewares/auth.middleware.js
+++ middlewares/auth.middleware.js
@@ -7,11 +7,13 @@
 import jwt from 'jsonwebtoken';
 
 export const verifyToken = (req, res, next) => {
-    const token = req.headers['authorization'];
+    const authHeader = req.headers['authorization'];
 
-    if (!token) {
+    if (!authHeader) {
         return res.status(401).json({ message: "Token no proporcionado ❌" });
     }
+
+    const token = authHeader.split(' ')[1];
 
     try {
         const decoded = jwt.verify(token, process.env.SECRET);
